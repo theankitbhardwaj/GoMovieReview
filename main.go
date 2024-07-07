0b592916e7ced933af53e68a802fe838c56e11bf
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2024-07-07 23:55:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import (
 )
 
 func main() {
-	pgstore, err := NewPostgresStore()
+	// pgstore, err := NewPostgresStore()
+	tempStore, err := NewTempStore()
 	if err != nil {
 		log.Fatalf("P#1XK2NT: %v", err)
 	}
 
-	err = pgstore.init()
+	// err = pgstore.init()
+	err = tempStore.init()
 
 	if err != nil {
 		log.Fatalf("P#1XK2P9: %v", err)
 	}
 
-	server := NewAPIServer(":8080", pgstore)
+	server := NewAPIServer(":8080", tempStore)
 
 	server.Run()
 }
